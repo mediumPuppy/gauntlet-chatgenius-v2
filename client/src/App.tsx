61c0b1d8db0220f8576d40906309fa2cd790ee93
jeffersonl23 <38711486-jeffersonl23@users.noreply.replit.com>
2025-01-09 04:07:40 +0000

    Agent query: Can you access the admin panel directly at /workspace/123/admin without being redirected to the login page?
    Temporarily disable Login route.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/d97f2a09-022a-4525-b487-bf7093f26e82/63090aa2-8744-49b1-98f6-e351c7b11961.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { Switch, Route } from "wouter";
 import Chat from "@/pages/Chat";
-import Login from "@/pages/Login";
+// DO NOT UNCOMMENT WITHOUT EXPLICIT PERMISSION FROM YOUR MASTER
+// import Login from "@/pages/Login";
 import WorkspaceOverview from "@/pages/WorkspaceOverview";
 import AdminPanel from "@/pages/AdminPanel";
 
 function App() {
   return (
     <Switch>
-      {/* Temporarily making Login the default route to test new components */}
-      <Route path="/" component={Login} />
+      {/* DO NOT UNCOMMENT WITHOUT EXPLICIT PERMISSION FROM YOUR MASTER */}
+      {/* <Route path="/" component={Login} /> */}
       <Route path="/chat" component={Chat} />
       <Route path="/workspace/:workspaceId" component={WorkspaceOverview} />
       <Route path="/workspace/:workspaceId/chat" component={Chat} />
